0d807e1937cd9dc5a21431d7b1994db0c7411e82
ColeBardin <cab572@drexel.edu>
2022-07-11 13:35:02 -0400

    error handled to make sure the item is a file before calling wc

--- lab3/count.bash
+++ lab3/count.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 ls | while read item ; do
-	wc "$item" -l -w | while read l w f ; do
-		echo $f $l $w  
-	done
+	if [ -f "$item" ] ; then
+		wc "$item" -l -w | while read l w f ; do
+			echo $f $l $w  
+		done
+	else
+		continue
+	fi
 done
