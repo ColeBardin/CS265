6e31a907ee4d7f09e6e429ef2cf0fe5837d449e2
ColeBardin <cab572@drexel.edu>
2022-07-12 09:46:10 -0400

    added counter for count.bash

--- lab3/count.bash
+++ lab3/count.bash
@@ -3,10 +3,13 @@
 echo "count.bash"
 echo "Filename Linecount WordCount:"
 echo
+
+count=0
 # Iterate through each item in current directory
-ls | while read item ; do
+while read item ; do
 	# If the item is of type 'file'
 	if [ -f "$item" ] ; then
+		((count=count+1))
 		# Produce wordcount and linecount for the file and store the values into variables f, l and w
 		wc "$item" -l -w | while read l w f ; do
 			# Print out the filename with its linecount and wordcount
@@ -16,4 +19,8 @@ ls | while read item ; do
 		# Continue if file is not of type f
 		continue
 	fi
-done
+done <<<$(ls -1)
+
+if (( $count == 0 )) ; then
+	echo "ERROR: No files found in current directory to count" ; exit
+fi
