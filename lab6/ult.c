c8c8c845ef0cc6b8bbd36ba80bcf9fa5dabe92b6
ColeBardin <cab572@drexel.edu>
2022-08-01 14:03:09 -0400

    added comments to ult.c

--- lab6/ult.c
+++ lab6/ult.c
@@ -3,16 +3,19 @@
 int main() {
 	int n, i;
 
+	/* Read int from user */
 	printf("Enter an integer: ");
 	if (scanf("%d", &n) != 1) {
 		fprintf(stderr, "ERROR: scanf did not match one integer from user input\n");
 		return -1;
 	}
 	while (n>0) {
+		/* Print out n number of * */
 		for (i=0; i<n; i++){
 			printf("*");
 		}
 		printf("\n");
+		/* Decrement n */
 		n--;
 	}
 	return 0;
