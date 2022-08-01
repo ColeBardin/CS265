39200f5c7613c2e50feab6eb8210c6062b12e283
ColeBardin <cab572@drexel.edu>
2022-08-01 13:50:36 -0400

    finished readlines.c file

--- lab6/readlines.c
+++ lab6/readlines.c
@@ -10,14 +10,27 @@ int main()
 {
 	char *buff = NULL ;
 	size_t len ;
-	int cnt = 0 ;
+	int cnt = 0;
+	double min, max, mean=0.0, curr;
+	int flag=0;
 
 	while( getline( &buff, &len, stdin ) > 1 )
 	{
+		curr = atof(buff);
 		++cnt ;
+		if (!flag) {
+			min=curr;
+			max=curr;
+			flag=1;
+		}
+		
+		if (curr < min) min=curr;
+		if (curr > max) max=curr;
+		mean = ( ((double)cnt-1.0)*mean + curr ) / (double)cnt ;
 	}
 
 	printf( "\nEnd of input detected.  I read %d lines.\n", cnt ) ;
+	printf( "Min:  %lf\nMax:  %lf\nMean: %lf\n", min, max, mean);
 
 	free( buff ) ;
 	return 0 ;
