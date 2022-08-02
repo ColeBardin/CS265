0db25722a8ff73017486319161ff4ae4888a76ea
ColeBardin <cab572@drexel.edu>
2022-08-02 10:25:05 -0400

    added TODOs for assignment2

--- assignment2/msg.c
+++ assignment2/msg.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int valid=0;
+
+int check_foo();
+int check_eep();
+int check_op();
+int check_ork();
+
+int main(int argc, char *argv[]) {
+	
+	char first;
+
+	/* TODO: handle arguments to determine input type */
+
+	/* TODO: Open file and error handle */
+
+	/* TODO: Get first char and determine message type */
+
+	/* TODO: Validate message (func prints message too!) */
 
 	return 0;
 }
