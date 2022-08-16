7133a32dac30f7b8ad21b568b96a121077b0a5c5
ColeBardin <cab572@drexel.edu>
2022-08-16 18:01:10 -0400

    working on readPeople, error in malloc

--- lab8/readPeople.c
+++ lab8/readPeople.c
@@ -54,18 +54,45 @@ int main( int argc, char **argv )
 	return 0 ;
 }
 
+/* readPeople function:
+Returns void
+Reads no more than 100 entries from file
+Entries contain first name, last name and age separated by newline
+Allocates memory on heap for each entry
+Populates a struct for each entry and adds it to given array
+Parameters:
+	person* a[]: 		Array of pointers to person structs
+	FILE* f: 			Opened file pointer to read from */
 void readPeople( person* a[], FILE* f ) 
 {
 	char *buff = NULL ;
 	size_t len ;
 	size_t cnt = 0 ;
 
+	person *temp;
 	while( getline( &buff, &len, f ) > 1 ) 
 	{
 		// We read a last name (with the newline)
 		// - get memory for person
+		temp = (person*)malloc(sizeof(*temp));
+		printf("Malloc called\n");
+		if (!temp) {
+			fprintf(stderr, "ERROR: Malloc failed while trying to readPeople\n");
+			return;
+		}
 		// - append to array
+		a[cnt] = temp;
+		// - copy string name manually since string.h is not included
+		for (int i=0; i<len; i++){
+			temp->last[i] = buff[i];
+		}
 		// - read next 2 lines, fill it in 
+		getline(&buff, &len, f);
+		for (int i=0; i<len; i++){
+			temp->first[i] = buff[i];
+		}
+		getline(&buff, &len, f);
+		temp->age = atoi(buff);
 
 		++cnt ;
 	}
@@ -76,4 +103,10 @@ void readPeople( person* a[], FILE* f )
 void freePeople( person* a[], size_t n ) 
 {
 	// Do NOT assume array is dense.  Check every element
+	for (int i=0; i<n; i++) {
+		if (a[i] != NULL) {
+			free(a[i]);
+			a[i] = NULL;
+		}
+	}
 }
